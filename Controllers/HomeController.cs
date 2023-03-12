a558623e1c10b31843d8b3ca35854850d9d47379
Dikachi_Dev <onyex11@gmail.com>
2023-03-12 08:28:23 +0100

    Added cart function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using PerfumeStore.Data;
 using PerfumeStore.Helpers;
 using PerfumeStore.Models;
@@ -32,11 +33,75 @@ namespace PerfumeStore.Controllers
                     CurrentPage = page,
                     TotalPages = totalPages,
                     Products = products
-                        .Skip((page - 1) * pagemax).Take(totalPages).ToList()
-                };              
+                    .Skip((page - 1) * pagemax).Take(pagemax).ToList()
+
+                };
+                string message = TempData["Message"] as string;
+                if (!string.IsNullOrEmpty(message))
+                {
+                    ViewBag.Message = message;
+                    TempData.Remove("Message");
+                }
                 return View(viewModel);
             }
             return Problem("Entity set 'PerfumeStoreContext.Product'  is null.");        
+        }
+        public IActionResult AddToCart(int productId)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var cart = Request.Cookies["cart"];
+            if (cart != null)
+            {
+                var cartList = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                var existingItem = cartList.FirstOrDefault(item => item.ProductId == productId);
+                if (existingItem != null)
+                {
+                    existingItem.Quantity++;
+                }
+                else
+                {
+                    cartList.Add(new CartItem
+                    {
+                        ProductId = productId,
+                        Name = product.Name,
+                        Price = product.Price,
+                        Quantity = 1
+                    });
+                }
+                Response.Cookies.Append("cart", JsonConvert.SerializeObject(cartList));
+            }
+            else
+            {
+                var cartList = new List<CartItem>
+        {
+            new CartItem
+            {
+                ProductId = productId,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = 1
+            }
+        };
+                Response.Cookies.Append("cart", JsonConvert.SerializeObject(cartList));
+            }
+            TempData["Message"] = "Continue";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Cart()
+        {
+            var cart = Request.Cookies["cart"];
+            if (cart != null)
+            {
+                var cartList = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                ViewBag.Cart = cartList;
+                return View();
+            }
+            return View();
         }
 
         public IActionResult Privacy()
